f08f0a9fbcdd49e9e3e372e4401b459ccc299e7a
KimSeungDeok <tmdejr1117@gmail.com>
2022-01-24 12:17:24 +0900

    공부 중

---
+++ syntax/variable2.js
@@ -0,0 +1,6 @@
+var test = 'Me';
+
+// var letter = 'Dear Me Lorem ipsum dolor sit amet, consectetur adipisc dear Me ing elit, sed do eiusmod tempor incididunt dear Me  ut labore et dolore magna aliqua. Ut e dear Me nim ad minim veniam, quis nostrud exer dear Me citation ullamco laboris nisi ut aliquip e dear Me x ea commodo consequat. Duis aute irur dear Me e dolor in reprehenderit in voluptat dear Me e velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+var letter = 'Dear'+ test +'  Lorem ipsum dolor sit amet, consectetur adipisc dear '+ test +' ing elit, sed do eiusmod tempor incididunt dear Me  ut labore et dolore magna aliqua. Ut e dear Me nim ad minim veniam, quis nostrud exer dear Me citation ullamco laboris nisi ut aliquip e dear '+ test +' x ea commodo consequat. Duis aute irur dear Me e dolor in reprehenderit in voluptat dear Me e velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+console.log(letter);
