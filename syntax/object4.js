dd5337a494c0beed1778cbebd1bc718dd3656d6b
KimSeungDeok <tmdejr1117@gmail.com>
2022-02-06 16:52:09 +0900

    node.js 스터디

---
+++ syntax/object4.js
@@ -0,0 +1,36 @@
+// var v1 = 'v1';
+
+// v1 = 'seungdeok';
+
+// var v2 = 'v2';
+
+var o = {
+    v1 : 'v1',
+    v2 : 'v2',
+    // f1 : function(){
+    //     console.log(o.v1);
+    // },
+    // f2 : function (){
+    //     console.log(o.v2);
+    // }
+    f1 : function(){
+        console.log(this.v1);
+    },
+    f2 : function (){
+        console.log(this.v2);
+    }
+}
+
+// function f1(){
+//     console.log(o.v1);
+// }
+
+// function f2(){
+//     console.log(o.v2);
+// }
+
+// f1();
+// f2();
+
+o.f1();
+o.f2();
